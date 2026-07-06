34e8f65e1ee873aeb12a0b7efa7296f46fc44974
Michael Cho <michael@michaelcho.dev>
2026-07-06 11:46:27 -0400

    mysql@8.0: remove dynamic caveat for documented config paths

--- Formula/m/mysql@8.0.rb
+++ Formula/m/mysql@8.0.rb
@@ -138,15 +138,6 @@ class MysqlAT80 < Formula
     init_data_dir "mysql", using: :mysql_initialize
   end
 
-  def post_install
-    if (my_cnf = ["/etc/my.cnf", "/etc/mysql/my.cnf"].find { |x| File.exist? x })
-      opoo <<~EOS
-        A "#{my_cnf}" from another install may interfere with a Homebrew-built
-        server starting up correctly.
-      EOS
-    end
-  end
-
   def caveats
     <<~EOS
       We've installed your MySQL database without a root password. To secure it run:
