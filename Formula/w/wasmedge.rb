ac0d194b873c92193ddca1d268561b65904722e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-06 13:11:25 +0000

    wasmedge 0.17.1

--- Formula/w/wasmedge.rb
+++ Formula/w/wasmedge.rb
@@ -1,8 +1,8 @@
 class Wasmedge < Formula
   desc "Lightweight, high-performance, and extensible WebAssembly runtime"
   homepage "https://WasmEdge.org/"
-  url "https://github.com/WasmEdge/WasmEdge/releases/download/0.17.0/WasmEdge-0.17.0-src.tar.gz"
-  sha256 "f4e341e4c8a1f9d8a67579ed91b447d363edecd3f6f592573eaddfcd993fae2e"
+  url "https://github.com/WasmEdge/WasmEdge/releases/download/0.17.1/WasmEdge-0.17.1-src.tar.gz"
+  sha256 "c8881a8c43407fc424ccd8586594a79068305b31c76aad0025efea9339be18e0"
   license "Apache-2.0"
   head "https://github.com/WasmEdge/WasmEdge.git", branch: "master"
 
